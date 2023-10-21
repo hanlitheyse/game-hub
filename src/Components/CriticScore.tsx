d8ef6cdc7eaeb733cf05a6bbd37f46627404db2e
hanlitheyse <hanlitheyse@gmail.com>
2023-10-22 08:23:12 +1000

    Display critic score

---
+++ src/Components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+  return (
+    <>
+      <Badge
+        fontSize={"14"}
+        paddingX={2}
+        borderRadius={"4"}
+        colorScheme={color}
+      >
+        {score}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScore;
