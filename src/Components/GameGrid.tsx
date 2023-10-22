c8249a29e22fbe33f5352b296a2674277cd68a3c
hanlitheyse <hanlitheyse@gmail.com>
2023-10-22 10:40:28 +1000

    Display genres

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = () => {
         <SimpleGrid
           columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
           padding={10}
-          spacing={10}
+          spacing={5}
         >
           {isLoading &&
             skeletons.map((skeleton) => (
